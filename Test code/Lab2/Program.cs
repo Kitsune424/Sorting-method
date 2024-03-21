05d717ddba576de69cbb2e6075d477a6ca795fd5
K1tsune <60034144+Kitsune424@users.noreply.github.com>
2024-03-21 10:28:18 +0300

    load project

---
+++ Test code/Lab2/Program.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Diagnostics;
+using Sortingoperations_library;
+
+namespace Lab_02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                int ID = 0;
+                int ID_copy = 0;
+                int[] _array = new int[0];
+                int[] _copyarray = new int[0];
+                Stopwatch stopwatch = new Stopwatch(); //cекундомер
+                ConsoleKeyInfo K;
+                do
+                {
+                    #region  Interface
+                    Console.Clear(); //очистка экрана перед выводом меню
+                    Console.WriteLine("1.Создать массив");
+                    Console.WriteLine("2.Вывести исходный массив");
+                    Console.WriteLine("3.Сортировка способ 1");
+                    Console.WriteLine("4.Сортировка способ 2");
+                    Console.WriteLine("5.Вывести отсортированный массив");
+                    Console.WriteLine("Esc. Выход из программы");
+                    #endregion
+
+                    K = Console.ReadKey(); //считывание кода вводимой клавиши
+                    switch (K.Key)
+                    {
+                        /*Кейс для создания массива*/
+                        case ConsoleKey.D1:
+                        case ConsoleKey.NumPad1:// если нажата клавиша с цифрой 1
+                            {
+                                ID = Sorting.ArrayID();
+                                Console.WriteLine("\nЗадайте размер массива:");
+                                int n = 0;
+                                bool n_t = int.TryParse(Console.ReadLine(), out n);
+
+                                if (n_t)
+                                {
+                                    if (n < 0)
+                                    {
+                                        Console.WriteLine("Размер массива не может быть равен нулю");
+                                    }
+                                    else
+                                    {
+                                        
+                                        _array = Sorting.Input(n);
+                                        break;
+                                    }
+                                }
+                                else
+                                {
+                                    Console.WriteLine("неверный тип размера массива");
+                                }
+                                break;
+                            }
+
+                        /*Кейс вывода исходного массива*/
+                        case ConsoleKey.D2:
+                        case ConsoleKey.NumPad2:// если нажата клавиша с цифрой 2
+                            {
+                                if (_array.Length != 0)
+                                {
+                                    Console.WriteLine("\nИскходный массив");
+                                    Console.WriteLine(Sorting.Output(_array));
+                                }
+                                else { Console.WriteLine("Нет готового массива"); }
+                                break;
+                            }
+
+                        /*Кейс для сортировкии копии исходного массива методом вставки*/
+                        case ConsoleKey.D3:
+                        case ConsoleKey.NumPad3:// если нажата клавиша с цифрой 3
+                            {
+                                if (_array.Length != 0)
+                                {
+                                    ID_copy = ID;
+                                    Console.WriteLine("\nСортировка методом вставки");
+                                    _copyarray = Sorting.CopyArray(_array);
+
+                                    stopwatch.Start();
+                                    Sorting.InsertMethod(_copyarray);
+                                    stopwatch.Stop();
+                                    Console.WriteLine($"Время выполнения сортировки: {stopwatch.Elapsed.TotalMilliseconds} мс");
+                                }
+                                else { Console.WriteLine("Нет массива для сортировки"); }
+                                break;
+                            }
+
+                        /*Кейс для сортировки копии исхоного массива методом слияния*/
+                        case ConsoleKey.D4:
+                        case ConsoleKey.NumPad4:// если нажата клавиша с цифрой 4
+                            {
+                                if (_array.Length != 0)
+                                {
+                                    ID_copy = ID;
+                                    Console.WriteLine("\nСортировка методом слияния");
+                                    _copyarray = Sorting.CopyArray(_array);
+
+                                    stopwatch.Start();
+                                    Sorting.MergeMethod(_copyarray);
+                                    stopwatch.Stop();
+                                    Console.WriteLine($"Время выполнения сортировки: {stopwatch.Elapsed.TotalMilliseconds} мс");
+                                }
+                                else { Console.WriteLine("Нет массива для сортировки"); }
+                                break;
+                            }
+
+                        /*Кейс для вывода отсортированного массива*/
+                        case ConsoleKey.D5:
+                        case ConsoleKey.NumPad5:// если нажата клавиша с цифрой 5
+                            {
+                                if (_copyarray.Length != 0 && ID_copy == ID)
+                                {
+                                    Console.WriteLine("\n" + Sorting.Output(_copyarray));
+                                }
+                                else
+                                { Console.WriteLine("\nМассив не был отсортирован"); }
+
+                                break;
+                            }
+                        default: break;
+                    }
+                    // приостанавливаем выполнение текущего потока на заданное число времени. Время измеряется в миллисекундах
+                    System.Threading.Thread.Sleep(2000); // 2,6сек.
+                }
+                while (K.Key != ConsoleKey.Escape);// цикл заканчивается, если нажата клавиша Esc
+
+            }
+            catch (Exception ex) { Console.WriteLine(ex.Message); }
+        }
+    }
+
+}
+
---
+++ Test code/sorting operations/Class1.cs
@@ -0,0 +1,180 @@
+﻿using System;
+using System.Data;
+using System.Diagnostics;
+using System.Globalization;
+
+namespace Sortingoperations_library
+{
+    public class Sorting
+    {
+        #region Interface integration
+        /// <summary>
+        /// Метод заполнения массива с задаваемым размером
+        /// </summary>
+        /// <param name="n"> размер массива </param>
+        /// <returns></returns>
+        public static int[] Input(int n)
+        {
+            int[] numbers = new int[n];
+            Random rnd = new Random();
+
+            for (int i = 0; i < numbers.Length; i++)
+                numbers[i] = rnd.Next(-1000, 1000);
+            return numbers;
+        }
+
+        public static int[] CopyArray(int[] arraytocopy)
+        {
+            int[] _copyarray = new int[arraytocopy.Length];
+            Array.Copy(arraytocopy, _copyarray, arraytocopy.Length);
+            return _copyarray;
+        }
+
+        /// <summary>
+        /// Формирование строки из одномерного массива
+        /// </summary>
+        /// <param name="array">строка</param>
+        public static string Output(int[] array)
+        {
+            string s = String.Empty;
+            for (int i = 0; i < array.Length; i++)
+            {
+                s = s + array[i].ToString() + " ";
+            }
+            s = s + "\n";
+            return s;
+        }
+
+        public static int ArrayID()
+        {
+            int ID = 0;
+            Random rnd = new Random();
+            ID = rnd.Next(1000, 9999);
+            return ID;
+        }
+        #endregion
+
+        #region Insert Method
+        /// <summary>
+        /// сортировка методом вставки 
+        /// </summary>
+        /// <param name="unsorted"> не отсортированный массив чисел </param>
+        public static void InsertMethod(int[] unsorted)
+        {
+            int N = unsorted.Length; //длина изначального массива
+            for (int i = 1; i < N; i++) //начиная с первого элемента мы условно делим массив на сортированную и не сортированную части
+            {
+                int _currentelement = unsorted[i]; //текущий рассматриваемый элемент
+                int j = i - 1;
+
+                #region большой некрасивый комментарий
+                /// берем рассматриваемый элемент и смотрим
+                /// соблюдается ли условие сортировки в массиве,
+                /// если элемент будет в позиции j+1
+                /// если рассматриваемый элемент оказывается меньше 
+                /// предыдущего, то мы двигаем элементы в право до тех пор
+                /// пока _currentelement в позиции j+1 не будет удовлетоворять отсорт. позиции
+                #endregion
+                while (j >= 0 && unsorted[j] > _currentelement)
+                {
+                    unsorted[j + 1] = unsorted[j]; //сдвиг в право
+                    j -= 1;
+                }
+                unsorted[j + 1] = _currentelement;
+            }
+        }
+        #endregion
+
+        #region Merge method
+        /// <summary>
+        /// Реализация сортировки методом слияния
+        /// </summary>
+        /// <param name="unsorted">не отсортированный массив чисел</param>
+        public static void MergeMethod(int[] unsorted)
+        {
+            int N = unsorted.Length; //длина изначального массива
+            if (N <= 1) { return; } //мы будем делить наш массив до того, пока он не будет разделенн до единичных массивов
+
+            int mid = N / 2;
+            
+            /*левый куос - половина от разделяемого (до середины)
+            правый кусок - другая половина от разделяемого (после середины)*/
+            int[] leftArray = new int[mid];
+            int[] rightArray = new int[N - mid];
+
+            int i = 0; //left array index pos
+            int j = 0; //right array index pos
+
+            /* бежим по индексам делимого массива
+            первую половину i-ых элементов гоним в левый
+            вторую половину i-ых элементов гоним в правый
+            j - нужен чтобы правые шли с 0 индекса*/
+            for (; i < N; i++)
+            {
+                if (i < mid) { leftArray[i] = unsorted[i]; }
+                else { rightArray[j] = unsorted[i]; j++; }
+            }
+
+            /*уходим в рекурсию для деления массива до состояния одиночных элементов
+            через деление каждой половины*/
+            MergeMethod(leftArray);
+            MergeMethod(rightArray);
+            Merge(leftArray, rightArray, unsorted);
+        }
+
+        /// <summary>
+        /// реализация слияния одинарных элементов
+        /// </summary>
+        /// <param name="left">левая половина делимого массива</param>
+        /// <param name="right">правая половина делимого массива</param>
+        /// <param name="unsorted">делимый массив</param>
+        static void Merge(int[] left, int[] right, int[] unsorted)
+        {
+            /*левая половина делимого в моменте массива равняется его половине
+            а правая всему остальному за ее исключением, так мы исключаем 
+            ситуации с нечетным кол-вом элементов*/
+            int leftSize = unsorted.Length / 2;
+            int rightSize = unsorted.Length - leftSize;
+
+            int i_pos = 0, l_pos = 0, r_pos = 0; //индексы для левой стороны, правой стороны и рассматриваемого массива
+
+            /*рассматривая разделенный массив, мы смотрим на первые индексы
+            его левой и правой стороны. На первую позицию мы записываем наименьший элемент*/
+            while (l_pos < leftSize && r_pos < rightSize) 
+            {
+                if (left[l_pos] < right[r_pos])
+                {
+                    unsorted[i_pos] = left[l_pos];
+                    i_pos++;
+                    l_pos++;
+                }
+                else
+                {
+                    unsorted[i_pos] = right[r_pos];
+                    i_pos++;
+                    r_pos++;
+                }
+            }
+            
+            /*По истечению первого While должно получится так, что либо прававя
+            либо левая сторона вышла по индексу за условие ИНДЕКС < РАЗМЕР
+            значит мы идем по отсавшимся элементам и берем наименьший из них*/
+            while (l_pos < leftSize) 
+            {
+                unsorted[i_pos] = left[l_pos];
+                i_pos++;
+                l_pos++;
+            }
+
+            while (r_pos < rightSize)
+            {
+                unsorted[i_pos] = right[r_pos];
+                i_pos++;
+                r_pos++;
+            }
+        }
+        #endregion
+    }
+}
+
+
